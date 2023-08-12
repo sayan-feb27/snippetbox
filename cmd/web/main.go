d62473584cf938fbe9670732954c23400c99af2b
Sayan B <sayan.badarchy@gmail.com>
2023-08-12 13:55:21 +0400

    add static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,9 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/detail", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.Handle("/static/", http.StripPrefix(
+		"/static",
+		http.FileServer(http.Dir("./ui/static/"))))
 
 	log.Print("Starting server on 4000 port")
 	err := http.ListenAndServe(":4000", mux)
